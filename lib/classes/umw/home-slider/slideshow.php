d80548f8feaec08e62e88141b501a38032c9adab
Curtiss Grymala <>
2021-04-06 10:16:30 -0400

    Debug JS

--- lib/classes/umw/home-slider/slideshow.php
+++ lib/classes/umw/home-slider/slideshow.php
@@ -28,7 +28,7 @@ class Slideshow {
 	/**
 	 * @var string the version number to append to any script/style files
 	 */
-	var $script_version = '0.1.35';
+	var $script_version = '0.1.36';
 	/**
 	 * @var int|null the length of time for which the transients should be valid
 	 */
